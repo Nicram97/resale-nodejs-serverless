ea35a6b2724a85e9b52b60fc1ed2935f0e80b4c9
Nicram97 <marcin952@gmail.com>
2024-12-06 18:34:19 +0100

    feat: handle user verification with OTP code;

--- user-service/app/models/UserModel.ts
+++ user-service/app/models/UserModel.ts
@@ -5,4 +5,9 @@ export interface UserModel {
     password: string;
     salt: string;
     user_type: 'BUYER' | 'SELLER';
+    first_name?: string;
+    last_name?: string;
+    profile_pic?: string;
+    verification_code?: number;
+    code_expiry?: string;
 }
