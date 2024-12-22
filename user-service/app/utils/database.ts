2ac5f83c206143154537e67c3c384822a534de53
Nicram97 <marcin952@gmail.com>
2024-12-22 18:03:59 +0100

    feat: update CRUD for cart/cart-items

--- user-service/app/utils/database.ts
+++ user-service/app/utils/database.ts
@@ -1,11 +1,19 @@
 import { Client } from "pg";
 
 export const DBCLient = () => {
+    // if db run from docker compose
+    // return new Client({
+    //     host: '127.0.0.1',
+    //     user: 'root',
+    //     database: 'user_service',
+    //     password: 'root',
+    //     port: 5432
+    // });
     return new Client({
-        host: '127.0.0.1',
-        user: 'root',
+        host: '***REMOVED***', 
+        user: 'user_service',
         database: 'user_service',
-        password: 'root',
+        password: '***REMOVED***',
         port: 5432
     });
 }
