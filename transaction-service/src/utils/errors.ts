e5290b1c3aa0e5317a7786a8375a5b73c10d1799
Nicram97 <marcin952@gmail.com>
2025-01-13 20:06:42 +0100

    feat: add transaction service with SNS + SQS and postgres DB;

---
+++ transaction-service/src/utils/errors.ts
@@ -0,0 +1,14 @@
+import { validate, ValidationError } from "class-validator";
+
+export const ValidateError = async (input: any): Promise<ValidationError[] | false> => {
+    const error = await validate(input, {
+        validationError: { 
+           target: true,
+        },
+    });
+
+    if (error.length) {
+        return error;
+    }
+    return false;
+}
