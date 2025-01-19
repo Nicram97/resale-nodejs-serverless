d0ce98878cdcf50e43ea6b530541f7b89a906019
Nicram97 <marcin952@gmail.com>
2025-01-19 22:15:25 +0100

    chore: code cleanup;

--- product-service/src/utils/db-connection.ts
+++ product-service/src/utils/db-connection.ts
@@ -5,7 +5,7 @@ mongoose.set('strictQuery', false);
 
 export default async (): Promise<mongoose.Mongoose> => {
     // if You are using docker mongodb instead of localhost use host.docker.internal + AWS SAM
-    const DB_URL = 'mongodb+srv://example:example@cluster0.u4e6z.mongodb.net/resale-nodejs-serverless' // 'mongodb://host.docker.internal:27017/resale-nodejs-serverless';
+    const DB_URL = 'mongodb://host.docker.internal:27017/resale-nodejs-serverless';
     try {
         const connection = await mongoose.connect(DB_URL, {
             autoIndex: true,
